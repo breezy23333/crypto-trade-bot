4c51d35b6e73a07e5356634600d4182c8ef8a448
breezy23333 <luvomaphela0@gmail.com>
2026-02-16 18:42:03 +0200

    Remove unused import from dashboard.py
    Remove unused import of execute_paper_trade.

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,6 @@ import csv
 import json
 import sqlite3
 from datetime import datetime 
-from paper_trader import execute_paper_trade
 import streamlit as st
 import pandas as pd
 import altair as alt
